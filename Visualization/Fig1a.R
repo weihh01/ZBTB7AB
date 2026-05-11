723159e10570f1801b5d50b02abedad87e82a60c
weihh01 <1227585167@qq.com>
2026-05-11 11:21:15 +0800

    Create Fig1a.R

---
+++ Visualization/Fig1a.R
@@ -0,0 +1,68 @@
+
+library(tidyverse)
+pdf("7A.pdf", width = 2.8, height = 2.8)
+# pdf("enrichment_HiC_scale_500bp.pdf", width = 3.2, height = 3.9, bg = "transparent")
+
+# df <- read.table("ChIP_SMC1_C2D5_all_CTCFmotif_Mm_profile.csv", sep = ",", header = F)
+# df <- as.data.frame(t(data.frame(df, row.names = 1)))
+
+df <- read.table("7A.tab", sep = "\t", header = F, skip = 2)
+df <- as.data.frame(t(data.frame(df[,-2], row.names = 1)))
+bins <- seq(from = -1000, to = 990, by = 10)
+df <- cbind(bins, df)
+
+
+p <- ggplot(data = df, mapping = aes(x = bins)) + 
+  geom_line(aes(y = df[,2]/1, color = "DMSO"), size = 1.2) +
+  geom_line(aes(y = df[,3]/1, color = "dTAG"), size = 1.2) +
+  # geom_line(aes(y = df[,6]/1, color = "Hi-TrAC"), size = 1.2) +
+  # geom_line(aes(y = df[,2]/1, color = "WT"), size = 1.2) +
+  scale_color_manual(values = c(
+    "DMSO" = "darkgoldenrod1",
+    "dTAG" = "steelblue"
+  )
+  )+ 
+  guides(color=guide_legend(
+                            # labels = c("DNase-C", expression(paste(italic('in situ'), ' Hi-C')), "Micro-C", "BL-Hi-C", "Hi-TrAC"),
+                            nrow=5, 
+                            byrow=TRUE,
+                            # label.hjust = 1,
+                            keywidth = 0.45
+                            )) +
+  xlab("ZBTB7A peak summit") + 
+  ylab("ZBTB7A ChIP-seq signal") +
+  labs(title = "") +
+  scale_x_continuous(
+    limits = c(-1000, 1000),
+    breaks = c(-1000, 0, 1000),
+    labels = c("-1kb", "0", "1kb"),
+    expand = expansion(mult = c(0, 0.03))
+  )+
+  scale_y_continuous(
+    limits = c(0, 2.5),
+    # sbreaks = seq(0,1,0.2),
+    # name = "DNase-C enrichment",
+    # sec.axis = sec_axis(~. *5, name="DNase-seq enrichment"),
+    expand = c(0,0)
+  ) + 
+  theme(
+    axis.text = element_text(color = "black",size = 14), #face = "bold",
+    axis.title = element_text(color = "black",size = 14),
+    panel.border = element_blank(), 
+    panel.grid = element_blank(),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 14),
+    panel.background = element_blank(), 
+    legend.position = c(0.8,0.55),
+    rect = element_rect(fill = NA, size=0, color = NA),
+    plot.background = element_rect(fill = NA, size=0, color = NA),
+    legend.background = element_rect(fill = NA, size=0, color = NA),
+    legend.key = element_rect(fill = NA, size=0, color = NA),
+    # plot.margin = margin(t = 0, r = 0, b = 0, l = 0, unit = "pt")
+  ) 
+
+
+
+p
+
+dev.off()
